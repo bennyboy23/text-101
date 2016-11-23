d4bb6cc0b7efc920a4b3ce8a8ea3a34a77dfe57d
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2016-11-23 18:35:02 +0000

    initial commit

---
+++ Assets/Script/TextController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "press down arrow to start";
+		text.horizontalOverflow = "wrap";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			text.text = "You are in a prison cell, and you want to escape. There are " +
+			"some dirty sheets on the bed, a mirror on the wall and a locked door \n" +
+			"press S for sheets \n" + "press M for mirror \n" + "press L for lock";
+		}
+		if(Input.GetKeyDown(KeyCode.UpArrow)){
+			text.text = "up key pressed";
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Text-101 a udemy lead, text based game

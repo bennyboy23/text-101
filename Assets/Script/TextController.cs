b9c7d7874495226801028a203a11eba7d92511aa
bennyboy23 <b.j.e.evans-12@alumni.lboro.ac.uk>
2016-11-23 21:52:53 +0000

    started work on states

--- Assets/Script/TextController.cs
+++ Assets/Script/TextController.cs
@@ -4,20 +4,36 @@ using UnityEngine.UI;
 
 public class TextController : MonoBehaviour {
 	public Text text;
+	private enum States
+	{
+		cell,
+		mirror,
+		sheets_0,
+		sheets_1,
+		lock_0,
+		lock_1
+	};
+	private States myState;
 	// Use this for initialization
 	void Start () {
+		myState = States.cell;
 		text.text = "press down arrow to start";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			text.text = "You are in a prison cell, and you want to escape. There are " +
+		if(myState == States.cell){
+			state_cell ();
+		}
+
+	}
+
+	void state_cell(){
+		text.text = "You are in a prison cell, and you want to escape. There are " +
 			"some dirty sheets on the bed, a mirror on the wall and a locked door \n" +
 			"press S for sheets \n" + "press M for mirror \n" + "press L for lock";
-		}
-		if(Input.GetKeyDown(KeyCode.UpArrow)){
-			text.text = "up key pressed";
-		}
+			if(Input.GetKeyDown(KeyCode.S)){
+				myState = States.sheets_0;
+			}
 	}
 }
